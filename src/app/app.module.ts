e573f8fb34e3da17a671c37ec9e80b2d463e25c3
Boris Heithecker <boris.heithecker@gmx.net>
2020-10-24 13:46:45 +0200

    Added locale de for number formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,6 +12,11 @@ import { AppComponent } from './app.component';
 import { TermSheetsModule } from './termsheets/termsheets.module';
 import { AppMainComponent } from './app.main';
 import { LessonsModule } from './lessons/lessons.module';
+
+//Needed to set de locale
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe, 'de');
 
 @NgModule({
   declarations: [
@@ -32,7 +37,9 @@ import { LessonsModule } from './lessons/lessons.module';
     LessonsModule,
     TermSheetsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "de" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
